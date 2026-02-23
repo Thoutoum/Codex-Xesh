47f76e22416b6e3e28c7014c2a2000e12c2c8c8b
Thoutoum <thoutoum@gmail.com>
2026-02-23 20:38:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ adrenaline_attaque = "https://github.com/Thoutoum/Codex-Xesh/blob/main/Attack_su
 blocage = "https://github.com/Thoutoum/Codex-Xesh/blob/main/Block.webp?raw=true"
 touche_critique = "https://github.com/Thoutoum/Codex-Xesh/blob/main/Critical_hit.webp?raw=true"
 adrenaline_defense = "https://github.com/Thoutoum/Codex-Xesh/blob/main/Defense_surge.webp?raw=true"
-touche = "https://github.com/Thoutoum/Codex-Xesh/blob/main/Hit.webp"
+touche = "https://github.com/Thoutoum/Codex-Xesh/blob/main/Hit.webp?raw=true"
 
 # On définit le style une fois pour toute l'app
 st.markdown("""
@@ -21,11 +21,11 @@ st.markdown("""
 
 def xesh_text(texte):
     # On remplace automatiquement des codes simples par des images
-    t = texte.replace("[adrenaline_attaque]", f'<img src="https://github.com/Thoutoum/Codex-Xesh/blob/main/Attack_surge.webp" class="icon">')
-    t = texte.replace("[blocage]", f'<img src="https://github.com/Thoutoum/Codex-Xesh/blob/main/Block.webp" class="icon">')
-    t = texte.replace("[touche_critique]", f'<img src="https://github.com/Thoutoum/Codex-Xesh/blob/main/Critical_hit.webp" class="icon">')
-    t = texte.replace("[adrenaline_defense]", f'<img src="https://github.com/Thoutoum/Codex-Xesh/blob/main/Defense_surge.webp" class="icon">')
-    t = texte.replace("[touche]", f'<img src="https://github.com/Thoutoum/Codex-Xesh/blob/main/Hit.webp" class="icon">')
+    t = texte.replace("[adrenaline_attaque]", f'<img src="https://github.com/Thoutoum/Codex-Xesh/blob/main/Attack_surge.webp?raw=true" class="icon">')
+    t = texte.replace("[blocage]", f'<img src="https://github.com/Thoutoum/Codex-Xesh/blob/main/Block.webp?raw=true" class="icon">')
+    t = texte.replace("[touche_critique]", f'<img src="https://github.com/Thoutoum/Codex-Xesh/blob/main/Critical_hit.webp?raw=true" class="icon">')
+    t = texte.replace("[adrenaline_defense]", f'<img src="https://github.com/Thoutoum/Codex-Xesh/blob/main/Defense_surge.webp?raw=true" class="icon">')
+    t = texte.replace("[touche]", f'<img src="https://github.com/Thoutoum/Codex-Xesh/blob/main/Hit.webp?raw=true" class="icon">')
   
     return st.markdown(t, unsafe_allow_html=True)
 
